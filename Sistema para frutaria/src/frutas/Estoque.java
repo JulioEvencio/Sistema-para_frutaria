91b14f6a2a3704cd84141c6ed7d728ab4cf6895f
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-04-12 18:58:39 -0300

    Classe do estoque adicionada

---
+++ Sistema para frutaria/src/frutas/Estoque.java
@@ -0,0 +1,72 @@
+package frutas;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public final class Estoque {
+
+    private HashMap<String, Fruta> armazenado = new HashMap<String, Fruta>();
+    private HashMap<String, Fruta> vendido = new HashMap<String, Fruta>();
+
+    public HashMap<String, Fruta> getArmazenado() {
+        return armazenado;
+    }
+
+    public void setArmazenado(HashMap<String, Fruta> armazenado) {
+        this.armazenado = armazenado;
+    }
+
+    public HashMap<String, Fruta> getVendido() {
+        return vendido;
+    }
+
+    public void setVendido(HashMap<String, Fruta> vendido) {
+        this.vendido = vendido;
+    }
+
+    public void guardar(Fruta fruta){
+        armazenado.put(fruta.getNome(), fruta);
+    }
+
+    // Serve para Adicionar uma fruta ou repor estoque
+    public void armazenar(Fruta fruta) {
+        Fruta frutaTemp = armazenado.get(fruta.getNome());
+        if (frutaTemp != null) {
+            fruta.setQuantidade(fruta.getQuantidade() + frutaTemp.getQuantidade());
+        }
+        guardar(fruta);
+    }
+
+    public void vender(ArrayList<Fruta> frutas) {
+        for (Fruta fruta : frutas) {
+            Fruta frutaTemp = armazenado.get(fruta.getNome());
+            if (frutaTemp != null) {
+                int sobraNoArmazem = frutaTemp.getQuantidade() - fruta.getQuantidade();
+                if (sobraNoArmazem >= 0) {
+                    frutaTemp.setQuantidade(sobraNoArmazem);
+                    guardar(frutaTemp);
+                    addVendidas(fruta);
+                    System.out.println("");
+                    System.out.println("Você comprou: " + fruta.getQuantidade() + " de " + fruta.getNome()
+                            + " total de: " + fruta.getQuantidade() * fruta.getValor());
+                    System.out.println("");
+                } else {
+                    System.out.println(
+                            "Erro na compra: Não temos a quantidade desejada para a fruta: " + fruta.getNome());
+                }
+            } else {
+                System.out.println("Erro na compra: Não existe a fruta: " + fruta.getNome());
+            }
+        }
+    }
+
+    // metodo para adicionar no hash que contem as frutas vendidas
+    public void addVendidas(Fruta fruta) {
+        Fruta frutaTemp = vendido.get(fruta.getNome());
+        if (frutaTemp != null) {
+            fruta.setQuantidade(fruta.getQuantidade() + frutaTemp.getQuantidade());
+        }
+        vendido.put(fruta.getNome(), fruta);
+    }
+
+}
