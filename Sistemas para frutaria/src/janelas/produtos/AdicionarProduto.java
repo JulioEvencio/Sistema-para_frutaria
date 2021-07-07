71354808596394934622b3ec24746fc8e372f364
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-07-07 17:26:06 -0300

    Interface da janela que adiciona um produto foi implementada

--- Sistemas para frutaria/src/janelas/produtos/AdicionarProduto.java
+++ Sistemas para frutaria/src/janelas/produtos/AdicionarProduto.java
@@ -9,12 +9,12 @@ package janelas.produtos;
  *
  * @author julio
  */
-public class AdicionarProdutos extends javax.swing.JDialog {
+public class AdicionarProduto extends javax.swing.JDialog {
 
     /**
      * Creates new form AdicionarProdutos
      */
-    public AdicionarProdutos(java.awt.Frame parent, boolean modal) {
+    public AdicionarProduto(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -144,20 +144,21 @@ public class AdicionarProdutos extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AdicionarProdutos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdicionarProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AdicionarProdutos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdicionarProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AdicionarProdutos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdicionarProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AdicionarProdutos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdicionarProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                AdicionarProdutos dialog = new AdicionarProdutos(new javax.swing.JFrame(), true);
+                AdicionarProduto dialog = new AdicionarProduto(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
