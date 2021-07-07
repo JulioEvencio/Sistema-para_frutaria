264284108b14208863e982c790752cefa489c322
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-07-07 18:04:18 -0300

    Pacote e classe do produto foram adicionados

---
+++ Sistemas para frutaria/src/produto/Produto.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package produto;
+
+/**
+ *
+ * @author julio
+ */
+public class Produto {
+
+    private String nome;
+    private int quantidade;
+    private double preco;
+
+    public Produto(String nome, int quantidade, double preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+}
