8b4104edbed913e7ae98aa87cc232d3f1a91a02f
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-07-07 18:30:50 -0300

    Classe que liga a interface com o resto do programa foi adicionada. Os métodos para adicionar, alterar e remover algum produto foram implementados

---
+++ Sistemas para frutaria/src/sistema/Sistema.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema;
+
+import produto.Produto;
+import java.util.HashMap;
+
+/**
+ *
+ * @author julio
+ */
+public class Sistema {
+
+    private static final HashMap<String, Produto> lista = new HashMap<>();
+
+    public static void adicionarProduto(String nome, int quantidade, double preco) throws ChaveInvalidaException {
+
+        if (lista.containsKey(nome)) throw new ChaveInvalidaException();
+
+        lista.put(nome, new Produto(nome, quantidade, preco));
+
+    }
+
+    public static void alterarProduto(String nome, int quantidade, double preco) throws ChaveInvalidaException {
+
+        if (!lista.containsKey(nome)) throw new ChaveInvalidaException();
+
+        lista.put(nome, new Produto(nome, quantidade, preco));
+
+    }
+
+    public static void removerProduto(String nome) throws ChaveInvalidaException {
+
+        if (!lista.containsKey(nome)) throw new ChaveInvalidaException();
+
+        lista.remove(nome);
+
+    }
+
+}
